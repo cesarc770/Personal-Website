34983726d81df19519500ad1c2d02b4894d54194
Cesar Caceres <cesarc770@gmail.com>
2018-01-23 23:32:06 -0700

    update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,27 +8,27 @@ module.exports = function(app){
 		res.sendFile(path.join(__dirname, "../public/index.html"));
 	});
 
-	app.get('/success', function(req, res){
-		res.sendFile(path.join(__dirname, "../public/success.html"));
-	});
+	// app.get('/success', function(req, res){
+	// 	res.sendFile(path.join(__dirname, "../public/success.html"));
+	// });
 
-	app.post('/message', function( req, res){
-		var mailOptions = {
-		  from: keys.mailUser,
-		  to: keys.mailUser,
-		  subject: 'New message from website from: '+ req.body.name,
-		  text: JSON.stringify(req.body)
-		};
+	// app.post('/message', function( req, res){
+	// 	var mailOptions = {
+	// 	  from: keys.mailUser,
+	// 	  to: keys.mailUser,
+	// 	  subject: 'New message from website from: '+ req.body.name,
+	// 	  text: JSON.stringify(req.body)
+	// 	};
 
-		transporter.sendMail(mailOptions, function(error, info){
-		  if (error) {
-		    console.log(error);
-		  } else {
-		    console.log('Email sent: ' + info.response);
-		    res.redirect('/success');
-		  }
-		});
+	// 	transporter.sendMail(mailOptions, function(error, info){
+	// 	  if (error) {
+	// 	    console.log(error);
+	// 	  } else {
+	// 	    console.log('Email sent: ' + info.response);
+	// 	    res.redirect('/success');
+	// 	  }
+	// 	});
 		
-	})
+	// })
 	
 };
