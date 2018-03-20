b34882de3f4f4571f44419dd5c291a6592516520
Cesar Caceres <cesarc770@gmail.com>
2018-03-19 18:28:12 -0600

    added function for screen size change

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,14 +1,22 @@
 window.onscroll = function() {myFunction()};
+window.onresize = function() {resizeScreen()};
 
+resizeScreen();
 // Get the header
-var header = document.getElementById("header");
+
+function resizeScreen() {
+	var header = document.getElementById("header");
+	document.getElementById("home").style.height = screen.height+ "px";
+}
+
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = header.offsetTop + screen.height;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+	var sticky = header.offsetTop + screen.height;
+  if (window.pageYOffset > sticky) {
   	header.style.display = "block";
     header.classList.add("sticky");
   } else {
